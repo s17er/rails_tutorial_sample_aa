a50d3b34818e1e77e0d8f4fe6d15e049505fdaf1
s17er <sinnerthesinner@gmail.com>
2016-01-25 23:22:37 +0900

    user_path(user)の特定ユーザのページの仮実装とテストを追加

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,9 +1,21 @@
 require 'rails_helper'
 describe "User pages" do
+
 	subject { page }
+
 	describe "signup page" do
 		before { visit signup_path }
+		
 		it { should have_content('Sign up') }
 		it { should have_title(full_title('Sign up')) }
+	end
+
+	describe "profile page" do
+		# ユーザ変数を作成するためのコードに置き換える
+		let(:user) { FactoryGirl.create(:user) }
+		before { visit user_path(user) }
+
+		it { should have_content(user.name) }
+		it { should have_title(user.name) }
 	end
 end
