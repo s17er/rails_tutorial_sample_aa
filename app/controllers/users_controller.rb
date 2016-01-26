99dade970c945e20b1a3cbd2f4d53d5f87f1c3fc
s17er <sinnerthesinner@gmail.com>
2016-01-26 23:15:06 +0900

    ユーザ登録画面で失敗する場合のケースに関する実装

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,5 +3,20 @@ class UsersController < ApplicationController
 		@user = User.find(params[:id])
 	end
 	def new
+		@user = User.new
+	end
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			# TODO 保存処理が成功した場合
+		else 
+			render "new"
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:name, :email, :password, :password_confirmation)
 	end
 end
